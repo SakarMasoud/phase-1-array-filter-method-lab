08c34b711ed143f82064d2a5f8867e2cb336ad07
Sakar Masoud <masoudsakar@gmail.com>
2022-10-21 14:59:48 +0300

    Sakar Masoud

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+const drivers1 = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+  function findMatching(array, name){
+    if ((Match) => Match.toLowerCase() === name.toLowerCase()){
+    return array.filter((Match) => Match.toLowerCase() === name.toLowerCase());
+    }
+  }
+
+  function fuzzyMatch(array, string){
+    if ((Match) => Match.toLowerCase().indexOf(string.toLowerCase()) === 0){
+    return array.filter((Match) => Match.toLowerCase().indexOf(string.toLowerCase()) === 0);
+    }
+  }
+
+  function matchName(array, name) {
+    return array.filter((Match) => Match.name === name);
+  }
